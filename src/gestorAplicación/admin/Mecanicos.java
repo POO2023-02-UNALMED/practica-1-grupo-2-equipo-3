e3e5c43772c9c8cbb147e4b090194fd13511cf2d
jsinij <141966551+jsinij@users.noreply.github.com>
2023-10-06 19:56:56 -0500

    :cow2:

---
+++ src/gestorAplicación/admin/Mecanicos.java
@@ -0,0 +1,13 @@
+package admin;
+import clientes.*;
+
+public class Mecanicos {
+	String nombre;
+	String clave; 
+	String afinidad; 
+	Vehiculo vehiculo;
+	Clientes clientes; 
+	Administrador administrador;
+	Orden solicitudes;
+	Integer Salario;
+}
--- src/gestorAplicación/clientes/TipoDaño.java
+++ src/gestorAplicación/clientes/TipoDaño.java
@@ -1,5 +1,18 @@
 package clientes;
 
 public class TipoDaño {
+	Vehiculo vehiculo;
+	String tipo; 
 
+
+	public TipoDaño(Vehiculo vehiculo, String tipo) {
+		this.vehiculo = vehiculo;
+		this.tipo = tipo;
+	}
+	
+	public TipoDaño() {
+		vehiculo = null;
+		tipo = "";
+	}
 }
+
