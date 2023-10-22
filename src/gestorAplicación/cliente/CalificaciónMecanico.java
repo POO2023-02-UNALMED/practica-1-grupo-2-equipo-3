4086b1c185a043b127623f18f451987dd063f660
jsinij <141966551+jsinij@users.noreply.github.com>
2023-10-22 16:53:02 -0500

    cxvxfsd
    :DDD

--- src/gestorAplicación/cliente/CalificaciónMecanico.java
+++ src/gestorAplicación/cliente/CalificaciónMecanico.java
@@ -1,10 +1,13 @@
 package cliente;
 import taller_mecanica.Mecanicos;
+import taller_mecanica.Administrador;
 
 public interface CalificaciónMecanico {
     
-	Mecanicos mecanicosActivosCliente();
+	Mecanicos mecanicoSeleccionado(int seleccion);
+	void mecanicosActivosCliente();
 	void calificarMecanico(Mecanicos mecanico, int calificación);
     void dejarComisionMecanico(Mecanicos mecanico, int comision);
-    void despedirporEncuesta(Mecanicos mecanico);
+    void despedirporEncuesta(Mecanicos mecanico, Administrador administrador);
+    void premiarPorEncuesta(Mecanicos mecanico, Administrador administrador);
 }
